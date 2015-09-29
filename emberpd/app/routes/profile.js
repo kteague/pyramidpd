0a6dc2b3773e7d57ea4c33ae16be464818dd9a6a
Kevin Teague <kevin@bud.ca>
2015-09-28 22:43:11 -0700

    todo app was ember 1.1, porting to ember 2.0

--- emberpd/app/routes/profile.js
+++ emberpd/app/routes/profile.js
@@ -1,5 +1,13 @@
+import Ember from 'ember';
+
 export default Ember.Route.extend({
-  model(params) {
-    return this.store.findRecord('profile', params.profile_id);
-  }
+    model(params) {
+      return this.store.findRecord('profile', params.profile_id);
+    }
 });
+
+//export default Ember.Route.extend({
+//  model(params) {
+//    return this.store.findRecord('profile', params.profile_id);
+//  }
+//});
